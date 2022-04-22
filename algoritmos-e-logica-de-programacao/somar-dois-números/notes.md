88f0a3ced8501a03994ad31449d4e0a31e358e48
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-21 23:04:31 -0300

    aula 2 Somar dois números e ajuste na aula 01

---
+++ algoritmos-e-logica-de-programacao/somar-dois-números/notes.md
@@ -0,0 +1,17 @@
+Boas práticas na escrita de variáveis
+
+- Em inglês 
+- Sem espaços ou acentuações especiais no nome
+- Não pode iniciar com número 
+- camelCase
+- Opcional - ponto e virgula servem para finalizar uma expressão de código
+
+[] Tipo de dado: Number
+[] typeof
+[] Operador matemático
+[] Manipulação de dados
+  [] Type conversion (type casting)
+  [] Number() (função construtora para converter um dado em número)
+  [] String() (função construtora para converter um dado em uma string)
+[] Ordem de precedência dos operadores
+[] Group operator
---
+++ algoritmos-e-logica-de-programacao/somar-dois-números/script.js
@@ -0,0 +1,25 @@
+/**
+ * Solicite 2 números, faça a soma deles
+ * e apresente o resultado final ao usuário
+ */
+
+// Minha solução antes da aula
+alert("Iremos somar os números")
+let numberOne = Number(prompt("Digite o primeiro número "))
+let numberTwo = Number(prompt("Digite o outro número "))
+let result = numberOne + numberTwo
+
+alert(`A soma dos números é: ${result}`)
+
+
+/**
+ * Solução da aula
+ * 
+alert("Iremos somar os números")
+let numberOne = prompt("Digite o primeiro número ")
+let numberTwo = prompt("Digite o outro número ")
+let result = Number(numberOne) + Number(numberTwo)
+
+alert("A soma dos números é: " + result)
+*/
+
