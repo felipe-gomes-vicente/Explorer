027dc1ccb5bb7219f31e802b7aff39761297fb7a
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-21 21:21:36 -0300

    ajuste na pasta css e introdução a Algoritmos e Lógica de Programação com JS

---
+++ algoritmos-e-logica-de-programacao/script.js
@@ -0,0 +1,18 @@
+// alert("Olá Mundo!")
+// Coméntario de linha unica
+
+/*
+ Comentario 
+ multi linha
+*/
+
+/*
+Encontre a Solução ao problema:
+
+Pergunte o nome do usuário e escreva a mensagem:
+"Olá, [nome do usuário]"
+*/
+
+alert("Se Apresente! ")
+let nome = prompt(" Qual é seu nome? ")
+alert(`Olá, Bem vindo ${nome}`)
