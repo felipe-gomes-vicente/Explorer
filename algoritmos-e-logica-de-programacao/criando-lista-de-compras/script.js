8e6a24f84f44436bd5f388f741571732d1eb8983
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-23 12:42:29 -0300

    ajuste dez items da lista

--- algoritmos-e-logica-de-programacao/criando-lista-de-compras/script.js
+++ algoritmos-e-logica-de-programacao/criando-lista-de-compras/script.js
@@ -7,7 +7,7 @@
 // Códio da aula
 let items = [];
 
-for(let item = 0; item < 2; item++) {
+for(let item = 0; item <= 10; item++) {
   let itemName = prompt("Digite o item " + (item + 1))
 
   items[item] = itemName
