edc5a5878ecc703978ea75bd2de7264c6f2d35ab
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-27 14:51:37 -0300

    Introdução à DOM do Browser

---
+++ javascript/projeto-06/script.js
@@ -0,0 +1,10 @@
+/**
+ * DOM - Document Object Model
+ * 
+ */
+
+let play = document.querySelector('.play')
+play.classList.add('hide')
+
+document.querySelector('.pause')
+.classList.remove('hide')
