4f6ac5e1258e0c3288780e5df1bb6fec31370927
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-27 22:27:38 -0300

    Avançando na lógica com pequenos passos

--- javascript/projeto-06/script.js
+++ javascript/projeto-06/script.js
@@ -17,11 +17,25 @@ const secondsDisplay = document.querySelector('.seconds')
 function countdown(){
   setTimeout(function(){
     let seconds =  Number(secondsDisplay.textContent)
+    let minutes =  Number(minutesDisplay.textContent)
 
-    if (seconds <= 0) {
-      seconds = 60
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    if(minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+      return
     }
-    secondsDisplay.textContent = seconds - 1
+
+    if(seconds <= 0) {
+      seconds = 2
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
 
     countdown()
   }, 1000)
@@ -60,5 +74,5 @@ buttonSoundOn.addEventListener('click', function(){
 
 buttonSet.addEventListener('click', function(){
   minutes = prompt("Quantos minutos? ")
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
