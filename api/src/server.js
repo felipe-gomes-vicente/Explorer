5594d4cdcb74cad95f935a414b40be01a371ad84
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-05-24 22:33:28 -0300

    Query Params

--- api/src/server.js
+++ api/src/server.js
@@ -5,10 +5,17 @@ const app = express();
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
 
-  response.send(`Mensagem ID: ${id}.
+  response.send(`
+    Mensagem ID: ${id}.
     Para o usuário: ${user}.
   `);
 });
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
