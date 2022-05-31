8de6daae4245ff0178fbd18abc8deb51458a9777
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-05-31 17:46:12 -0300

    display note and adjust register note

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -24,13 +24,27 @@ class NotesController {
       return {
         note_id,
         name,
-        url_id
+        user_id
       }
     });
 
     await knex("tags").insert(tagsInsert)
 
     response.json();
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("notes").where({ id }).first();
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+    return response.json({
+      ...note,
+      tags,
+      links
+    });
   }
 }
 
