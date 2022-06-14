cbf0da2c937dcd2b346d3587e871a381979cd5c0
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-14 16:56:21 -0300

    button text and finish page details

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,29 +1,48 @@
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Button title="Voltar" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut esse ad
+            magni, molestiae mollitia nobis maiores recusandae ipsum sunt odit 
+            suscipit provident. Ullam maxime veritatis sed in maiores explicabo 
+            minima!
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
