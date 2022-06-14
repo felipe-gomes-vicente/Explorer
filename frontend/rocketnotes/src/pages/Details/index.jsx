14a6442f5e37aecdefb0452d6d8ec9ee9be6b968
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-14 16:10:54 -0300

    component tag

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 import { Container, Links } from "./styles";
 
@@ -15,6 +16,11 @@ export function Details() {
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
           
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
 
       <Button title="Voltar" />
