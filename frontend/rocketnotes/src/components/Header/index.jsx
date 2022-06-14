407bbd5f044fb8f841e1050694823dc582d813d3
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-13 21:32:33 -0300

    user image

--- frontend/rocketnotes/src/components/Header/index.jsx
+++ frontend/rocketnotes/src/components/Header/index.jsx
@@ -1,10 +1,20 @@
 
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
   return (
     <Container>
-      
+      <Profile>
+        <img 
+          src="https://github.com/felipe-gomes-vicente.png"
+          alt="Foto do usuário" 
+        />
+        
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Felipe Vicente</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
