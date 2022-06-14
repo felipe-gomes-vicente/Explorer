8354f8601ad1add64a123efd6597c100bccdb07c
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-14 11:41:01 -0300

    children

--- frontend/rocketnotes/src/pages/Details/index.jsx
+++ frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,12 +1,21 @@
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          
+        </Links>
+      </Section>
 
       <Button title="Voltar" />
     </Container>
