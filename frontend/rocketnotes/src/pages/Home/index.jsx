fef47336accd9d7d207cda5f03e606d373e75c18
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-15 08:53:44 -0300

    input page Home

--- frontend/rocketnotes/src/pages/Home/index.jsx
+++ frontend/rocketnotes/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
-import { Header } from "../../components/Header";
 import { FiPlus } from "react-icons/fi";
+import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Input } from "../../components/Input";
 
 import {  Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
@@ -20,7 +21,7 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título" />
       </Search>
 
       <Content>
