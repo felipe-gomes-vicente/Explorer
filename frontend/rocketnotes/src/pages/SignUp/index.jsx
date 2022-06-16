2254737257b714bb8224c71cf8ec757a63037f9d
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-16 08:49:40 -0300

    created page SignUp

--- frontend/rocketnotes/src/pages/SignUp/index.jsx
+++ frontend/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,18 +1,26 @@
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import { Background, Container, Form } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
   return(
     <Container>
+      <Background />
+
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Cria sua conta</h2>
+
+        <Input 
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
 
         <Input 
           placeholder="E-mail"
@@ -26,12 +34,10 @@ export function SignIn() {
           icon={FiLock}
         />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <a href="#"> Criar conta</a>
+        <a href="#"> Voltar para login</a>
       </Form>
-
-      <Background />
     </Container>
   );
 }
