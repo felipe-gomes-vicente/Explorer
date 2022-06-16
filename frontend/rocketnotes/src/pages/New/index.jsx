a653bc3086da6952bd8ba44b1d6632f2bb30d16c
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-16 11:21:27 -0300

    component TextArea

--- frontend/rocketnotes/src/pages/New/index.jsx
+++ frontend/rocketnotes/src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
 
 import { Container, Form } from "./styles";
 
@@ -15,8 +16,8 @@ export function New() {
             <a href="/">Voltar</a>
           </header>
 
-          <Input />
-          
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
         </Form>
       </main>
     </Container>
