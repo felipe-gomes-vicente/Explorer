d3a153ddb07167c751022376b0d85077d99d6006
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-07-01 14:01:01 -0300

    SignUp

--- frontend/rocketnotes/src/pages/SignIn/index.jsx
+++ frontend/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
@@ -8,9 +9,14 @@ import { Button } from "../../components/Button";
 import { Background, Container, Form } from "./styles";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const data = useAuth();
-  console.log(" Contexto =>", data)
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
 
   return(
     <Container>
@@ -24,15 +30,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register"> Criar conta</Link>
       </Form>
