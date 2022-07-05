8e1bfe451d927c8e02e8d6b418e00436422e9df2
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-07-05 18:25:29 -0300

    loading tags

--- frontend/rocketnotes/src/pages/Home/index.jsx
+++ frontend/rocketnotes/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { FiPlus } from "react-icons/fi";
 
 import { Header } from "../../components/Header";
@@ -6,9 +7,22 @@ import { Input } from "../../components/Input";
 import { Section } from "../../components/Section";
 import { Note } from "../../components/Note";
 
+import { api } from "../../services/api";
+
 import {  Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <Container>
       <Brand>
@@ -18,9 +32,16 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+        <li><ButtonText title="Todos" isActive /></li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name}
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
