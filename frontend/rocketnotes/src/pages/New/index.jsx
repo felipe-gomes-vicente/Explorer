3d64fc1e6732967ea97c5f419247f2c26aa57e6d
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-07-05 17:55:59 -0300

    Validating note fields

--- frontend/rocketnotes/src/pages/New/index.jsx
+++ frontend/rocketnotes/src/pages/New/index.jsx
@@ -43,6 +43,19 @@ export function New() {
   }
 
   async function handleNewNote() {
+
+    if (!title) {
+      return alert('Digite o título da nota!');
+    }
+    
+    if(newLink) {
+        return alert("Você deixou um link no campo para adicionar! Clique para adicionar ou deixe o campo vazio.")
+    }
+    
+    if(newTag) {
+      return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar.")
+    }
+
     await api.post('/notes', {
       title,
       description,
